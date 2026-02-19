c1f539ca931965dbb5cc9d187d54baea95ef1aed
martigiron01 <marti.giron01@estudiant.upf.edu>
2026-02-19 16:56:53 +0100

    backend

---
+++ src/main/java/api/Bicing.java
@@ -0,0 +1,20 @@
+package api;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
+public class Bicing {
+    // Load environment variables
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String TOKEN = dotenv.get("OPEN_DATA_API_TOKEN");
+    private static final String API_URL = dotenv.get("OPEN_DATA_API_URL");
+
+    public static Data fetchFromAPI() {
+        return ClientBuilder.newClient()
+                .target(API_URL)
+                .request(MediaType.APPLICATION_JSON)
+                .header("Authorization", TOKEN)
+                .get(Data.class);
+    }
+}
