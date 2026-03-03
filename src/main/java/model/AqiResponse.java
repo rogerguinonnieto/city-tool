be8b3f2bf15052cefdba9e3a236c941fef54968c
martigiron01 <marti.giron01@estudiant.upf.edu>
2026-03-03 19:40:29 +0100

    json ignore

--- src/main/java/model/AqiResponse.java
+++ src/main/java/model/AqiResponse.java
@@ -1,5 +1,8 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AqiResponse {
     private String status;
     private AqiData data;
--- src/main/java/model/IpInfo.java
+++ src/main/java/model/IpInfo.java
@@ -1,5 +1,8 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class IpInfo {
     private String status;
     private String city;
