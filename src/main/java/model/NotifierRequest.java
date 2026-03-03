d0575a63b2a17ed86ec404a21396d29179e34ab3
Roger Guinon <roger.guinon@gmail.com>
2026-03-03 18:11:37 +0100

    Notifier service implementation

--- src/main/java/model/NotifierRequest.java
+++ src/main/java/model/NotifierRequest.java
@@ -1,13 +1,13 @@
 package model;
 
 public class NotifierRequest {
-    private String phone;
-    private String ip; 
+    private int phone;
+    private String ip;
 
     public NotifierRequest() {}
 
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public int getPhone() { return phone; }
+    public void setPhone(int phone) { this.phone = phone; }
 
     public String getIp() { return ip; }
     public void setIp(String ip) { this.ip = ip; }
